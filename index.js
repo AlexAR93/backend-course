01b5913ed285949fc97325d50167aeb23580be34
aleexrodriguez93@gmail.com <aleexrodriguez93@gmail.com>
2022-10-19 23:36:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+class Contenedor {
+    constructor(alto, ancho) {
+      this.alto = alto;
+      this.ancho = ancho;
+    }
+
+    save(object){
+        // Recibe objeto y lo guarda.
+    }
+    getById(Number){
+        // Recibe id y devuelve el objeto con ese id, o null si no está.
+    }
+    getAll(){
+        // Devuelve un array con los objetos presentes.
+    }
+    deleteById(Number){
+        // Elimina el objeto con el id buscado
+    }
+    deleteAll(){
+        // Elimina todos los objetos presente
+    }
+  }
+
+let productsContainer={
+    id:(),
+    tittle:(),
+    price:(),
+    thumbnail:
+}
